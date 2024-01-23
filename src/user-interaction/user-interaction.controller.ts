e0aac668d96ce3e035b7f74d8970ddbc39530e74
蔡震 <1994370030@qq.com>
2024-01-23 20:47:38 +0800

    createinteraction

--- src/user-interaction/user-interaction.controller.ts
+++ src/user-interaction/user-interaction.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { PayLoadType } from 'src/auth/payload.type';
 import { UserInteractionService } from './user-interaction.service';
-
+@UseGuards(AuthGuard)
+@Controller('shop')
 @Controller('user-interaction')
 export class UserInteractionController {
   constructor(
     private readonly userInteractionService: UserInteractionService,
   ) {}
+
+  @Post('createInteraction')
+  async createInteraction(
+    @Request() req: { user: PayLoadType },
+    @Body()
+    body: {
+      productId: string;
+      isLike: boolean;
+      comment: string;
+    },
+  ) {
+    return this.userInteractionService.createInteraction({
+      userId: req.user.userid,
+      ...body,
+    });
+  }
 }
